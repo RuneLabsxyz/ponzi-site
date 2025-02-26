85e01060688ed8b5d4013fc0c09ae39bd35eb2e5
hadouin <hadouinleroy@gmail.com>
2025-02-26 19:03:56 +0530

    hide tweaker panel

--- src/lib/components/main-canvas.svelte
+++ src/lib/components/main-canvas.svelte
@@ -4,6 +4,7 @@
   import { Mesh } from "three";
   import HeroScene from "./hero-scene.svelte";
   import PixelatedRender from "./pixelated-render.svelte";
+  import { onMount } from "svelte";
 
   const mesh = new Mesh();
 
@@ -14,59 +15,67 @@
   let rotateOffset = $state(0.21);
   let scaleOffset = $state(1);
   let translateOffset = $state(1);
+
+  let dev = $state(false);
+
+  onMount(() => {
+    dev = window.location.hash == "#dev";
+  });
 </script>
 
-<Pane position="draggable" title="outline effect">
-  <Slider
-    bind:value={granularity}
-    label="granularity"
-    min={1}
-    max={20}
-    step={1}
-  />
-  <Slider
-    bind:value={rotateOffset}
-    label="rotate offset"
-    min={-Math.PI}
-    max={Math.PI}
-    step={0.01}
-  />
-  <Slider
-    bind:value={rotateFactor}
-    label="rotate factor"
-    min={0}
-    max={10}
-    step={0.1}
-  />
-  <Slider
-    bind:value={scaleOffset}
-    label="scale offset"
-    min={0}
-    max={10}
-    step={0.1}
-  />
-  <Slider
-    bind:value={scaleFactor}
-    label="scale factor"
-    min={0}
-    max={10}
-    step={0.1}
-  />
-  <Slider
-    bind:value={translateOffset}
-    label="translate offset"
-    min={-1}
-    max={1}
-    step={0.01}
-  />
-  <Slider
-    bind:value={translateFactor}
-    label="translate factor"
-    min={0}
-    max={10}
-    step={0.1}
-  />
-</Pane>
+{#if dev}
+  <Pane position="draggable" title="outline effect">
+    <Slider
+      bind:value={granularity}
+      label="granularity"
+      min={1}
+      max={20}
+      step={1}
+    />
+    <Slider
+      bind:value={rotateOffset}
+      label="rotate offset"
+      min={-Math.PI}
+      max={Math.PI}
+      step={0.01}
+    />
+    <Slider
+      bind:value={rotateFactor}
+      label="rotate factor"
+      min={0}
+      max={10}
+      step={0.1}
+    />
+    <Slider
+      bind:value={scaleOffset}
+      label="scale offset"
+      min={0}
+      max={10}
+      step={0.1}
+    />
+    <Slider
+      bind:value={scaleFactor}
+      label="scale factor"
+      min={0}
+      max={10}
+      step={0.1}
+    />
+    <Slider
+      bind:value={translateOffset}
+      label="translate offset"
+      min={-1}
+      max={1}
+      step={0.01}
+    />
+    <Slider
+      bind:value={translateFactor}
+      label="translate factor"
+      min={0}
+      max={10}
+      step={0.1}
+    />
+  </Pane>
+{/if}
 
 <section class="Container fixed top-0 right-0 bottom-0 left-0">
   <Canvas>
